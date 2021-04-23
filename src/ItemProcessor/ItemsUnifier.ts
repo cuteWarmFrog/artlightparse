408237ce3b9694cc4f792be3e76a4b92c0a57b42
cuteWarmFrog <livshits.victor@gmail.com>
2021-04-23 22:27:53 +0300

    unifies separate items in one json

---
+++ src/ItemProcessor/ItemsUnifier.ts
@@ -0,0 +1,19 @@
+export class ItemsUnifier {
+
+
+    unifyItems(items: Array<any>): Array<any> {
+
+        let allSpecs: any = [];
+        items.forEach((item: any) => allSpecs.push(...Object.keys(item)));
+        allSpecs = new Set(allSpecs);
+
+        items.forEach((item: any) => {
+            allSpecs.forEach((spec: any) => {
+                if (!item.hasOwnProperty(spec))
+                    item[spec] = '';
+            })
+        });
+
+        return items;
+    }
+}
