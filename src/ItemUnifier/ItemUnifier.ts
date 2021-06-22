6735240b39fbf5314a1d1631dd2fb6fb78931bca
cuteWarmFrog <livshits.victor@gmail.com>
2021-06-22 22:57:05 +0300

    There are some duplicates in the site (backend fucked up), this code removes them

--- src/ItemUnifier/ItemUnifier.ts
+++ src/ItemUnifier/ItemUnifier.ts
@@ -13,6 +13,24 @@ export class ItemUnifier {
             })
         });
 
-        return items;
+        return this.makeUnique(items);
+    }
+
+    private makeUnique(items: Array<any>): Array<any> {
+        const IDs = this.getIDs(items);
+        const uniqueIDs = Array.from(new Set(IDs));
+
+        return uniqueIDs.reduce<Array<any>>((acc: Array<any>, id: string) => {
+            const ourItem = items.find((item: any) => item['ID'] === id)
+            return acc.concat([ourItem]);
+        }, [])
+    }
+
+    private getIDs(json: Array<object>): Array<string> {
+        const ids = json.reduce<Array<string>>((acc: Array<string>, el: any) => {
+            const id = el['ID'];
+            return acc.concat([id]);
+        }, [])
+        return Array.from(ids);
     }
 }
